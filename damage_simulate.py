8bf60fc3566807072a510b9b3ff2706792e171cf
golbang2 <32033086+golbang2@users.noreply.github.com>
2022-11-28 23:05:22 +0900

    add earhart

--- damage_simulate.py
+++ damage_simulate.py
@@ -91,6 +91,7 @@ class Chanzhi:
         
         self.next_attack = self.elapsed + 1/(self.speed*0.01)
         self.dmg += dmg
+        print('atk',dmg)
         self.next_active-=1/(self.speed*0.01)
         return dmg
     
@@ -104,9 +105,11 @@ class Chanzhi:
         self.next_attack = self.elapsed + 1/(self.speed*0.01) 
         self.buff -= 1/(self.speed*0.01) 
         self.next_active = self.active_cooldown
+        self.dmg += dmg
+        print('triple',dmg)
+        
         if self.buff<0:
             self.close_skill()
-        self.dmg += dmg
         return dmg
         
     def close_skill(self):
@@ -114,6 +117,7 @@ class Chanzhi:
         self.psv_stack = 0
         self.dmg += dmg
         self.next_active = self.active_cooldown
+        print('active',dmg)
         return dmg
     
     def action(self):
@@ -127,16 +131,84 @@ class Chanzhi:
         if self.elapsed>=self.duration:
             self.done = 1
         
+class Earhart:
+    def __init__(self,target = 1, duration = 30):
+        self.atk = 402
+        self.speed = 115
+        self.power = 588
+        self.crit_p = 0.12
+        self.crit_m = 1.5
+        
+        self.next_attack = 1/(self.speed*0.01)
+        self.active_cooldown = 15
+        self.next_active = 5
+        
+        self.elapsed = 0
+        self.dmg = 0
+        self.target = target
+        self.done = 0
+        self.active_buff = 0
+        self.passive_buff = 0
+        self.duration = duration
+        
+    def attack(self):
+        self.elapsed = self.next_attack
+        if self.passive_buff>0:
+            self.passive_buff -=1/(self.speed*0.01)
+        dmg = self.atk
+        if np.random.random()<self.crit_p:
+            dmg = int(dmg * self.crit_m)
+            self.next_active -= 3
+            self.passive_buff = 3
+            self.speed = 165
+        self.next_attack = self.elapsed + 1/(self.speed*0.01)
+        self.dmg += dmg
+        self.next_active -= 1/(self.speed*0.01)
+        return dmg
+
+    def buff_attack(self):
+        self.elapsed = self.next_attack
+        if self.passive_buff>0:
+            self.passive_buff -=1/(self.speed*0.01)
+        dmg = int(self.power * 1.2)
+        if np.random.random()<self.crit_p:
+            dmg = int(dmg * self.crit_m)
+            self.next_active -= 3
+            self.passive_buff = 3
+            self.speed = 165
+        self.next_attack = self.elapsed + 1/(self.speed*0.01) 
+        self.active_buff -= 1/(self.speed*0.01)
+        self.dmg += dmg
+        return dmg
+    
+    def action(self):
+        if self.next_active<0:
+            self.active_buff = 16
+            self.next_active = 15
+            dmg = self.power * 2 * self.target
+            self.dmg += dmg
+            
+        if self.passive_buff<0:
+            self.speed = 115
+            
+        if self.active_buff>0:
+            self.buff_attack()
+        else:
+            self.attack()
+            
+        if self.elapsed>=self.duration:
+            self.done = 1
 
 if __name__=='__main__':
     duration = 30
-    target = 1
-    iteration = 1000
+    target = 2
+    iteration = 1
     dmg = 0
     
     for i in range(iteration):
         #doll = Hubble(target = target, duration = duration)
-        doll=Chanzhi(target = target,duration = duration)
+        doll = Chanzhi(target = target,duration = duration)
+        #doll = Earhart(target = target,duration = duration)
         while not doll.done:
             doll.action()
         dmg += doll.dmg/duration
